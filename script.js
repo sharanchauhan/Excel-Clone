2e717f0f05d535211bc2b0809c7b94ba38478abc
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-05-24 22:43:04 +0530

    Rows and columns done and styled using css and javascript

---
+++ script.js
@@ -0,0 +1,18 @@
+let cellsContent=document.querySelector(".cells-content");
+
+function initCells()
+{
+    let cell="";
+    for(let i=0;i<100;i++)
+    {
+        cell+="<div class='row'>";
+        for(let j=0;j<26;j++)
+        {
+            cell+="<div class='cell'></div>";
+        }
+        cell+="</div>";
+    }
+    cellsContent.innerHTML=cell;
+}
+
+initCells();
