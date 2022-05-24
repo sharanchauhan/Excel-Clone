481d0018cc1e04ef9ad68ec78075c2a76ed4f582
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-05-24 23:27:41 +0530

    left column, top row and top left box added and styled using CSS and Javascript

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-let cellsContent=document.querySelector(".cells-content");
+let cellsContentDiv = document.querySelector(".cells-content");
 
-function initCells()
-{
-    let cell="";
-    for(let i=0;i<100;i++)
-    {
-        cell+="<div class='row'>";
-        for(let j=0;j<26;j++)
-        {
-            cell+="<div class='cell'></div>";
-        }
-        cell+="</div>";
+
+function initCells(){
+    let cellsContent = "<div class='top-left-cell'></div>";
+    //top-row
+    cellsContent+="<div class='top-row'>"
+    for(let i=0;i<26;i++){
+        cellsContent+=`<div class='top-row-cell'>${String.fromCharCode(65+i)}</div>`;
     }
-    cellsContent.innerHTML=cell;
+    cellsContent+="</div>"
+
+    //left-col
+    cellsContent+="<div class='left-col'>"
+    for(let i=0;i<100;i++){
+        cellsContent+=`<div class='left-col-cell'>${i+1}</div>`
+    }
+    cellsContent+="</div>"
+    cellsContent+="<div class='cells'>"
+    for(let i=0;i<100;i++){
+        cellsContent+="<div class = 'row'>";
+        //column
+        for(let j=0;j<26;j++){
+            cellsContent+="<div class = 'cell' contentEditable></div>"
+        }
+        cellsContent+="</div>"
+    }
+    cellsContent+="</div>"
+    cellsContentDiv.innerHTML = cellsContent;
 }
 
 initCells();
