89c7566f0d7d9ee5982ddd14eaace8cfb2de6616
“sharanchauhan” <90271175+sharanchauhan@users.noreply.github.com>
2022-05-28 23:10:06 +0530

    Added custom attribute rowId and colId to update the address-content class on clicking and styled the address-content in style.css

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let topRow=document.querySelector(".top-row");
 let leftCol=document.querySelector(".left-col");
 let topLeftCell=document.querySelector(".top-left-cell");
+let allCells=document.querySelectorAll(".cell");
+let addressInput=document.querySelector("#address");
 
 cellsContentDiv.addEventListener("scroll",function(e)
 {
@@ -11,3 +13,14 @@ cellsContentDiv.addEventListener("scroll",function(e)
     topLeftCell.style.top=scrollFromTop+"px";
     topLeftCell.style.left=scrollFromLeft+"px";
 });
+
+for(let i=0;i<allCells.length;i++)
+{
+    allCells[i].addEventListener("click",function(e)
+    {
+        let rowId=Number(e.target.getAttribute("rowId"));
+        let colId=Number(e.target.getAttribute("colId"));
+        let address=String.fromCharCode(65+colId)+(rowId+1)+"";
+        addressInput.value=address;
+    })
+}
